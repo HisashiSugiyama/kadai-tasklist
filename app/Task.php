8310c2d3c6aa97d4aecfc3f53c2a9b3856f0ea1c
hisashi.sugiyama <sugidon@gmail.com>
2020-09-21 11:51:33 +0000

    kadai6

--- app/Task.php
+++ app/Task.php
@@ -8,11 +8,5 @@ class Task extends Model
 {
     protected $fillable = ['content'];
 
-    /**
-     * この投稿を所有するユーザ。（ Userモデルとの関係を定義）
-     */
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+
 }
